224bd1651477972552dcd12b87166b415fa8d8fe
NAPIER-MAIL\40329070 <40329070@live.napier.ac.uk>
2017-09-15 14:26:29 +0100

    This is a second Test

--- ProjectEuler/Program.cs
+++ ProjectEuler/Program.cs
@@ -62,6 +62,20 @@ namespace ProjectEuler
         
         }
 
+        public void problem3()
+
+        {
+            /* The prime factors of 13195 are 5, 7, 13 and What is the largest prime factor of the number 600851475143 ? */
+
+            //This is an example commment
+
+
+
+
+
+        }
+
+
 
 
 
