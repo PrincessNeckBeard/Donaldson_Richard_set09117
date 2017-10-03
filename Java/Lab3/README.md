d668dc71a5013a022993e6f00909be309bda0a4e
RICHARDD\richa <bearofthesea@gmail.com>
2017-10-03 15:20:54 +0100

    Added Lab3

---
+++ Java/Lab3/README.md
@@ -0,0 +1,4 @@
+# set09117
+
+Repository for code related to my Algoritms and Data Structures module 
+for my BEng Software Engineering degree. Java & C#
---
+++ Java/Lab3/src/hashmap/ExampleHashMap.java
@@ -0,0 +1,35 @@
+package hashmap;
+
+import java.util.*;
+public class ExampleHashMap {
+	HashMap<String, String> map = new HashMap<String, String>();
+	
+	public void build() {
+		
+		map.put("Richard", "0123456");
+		map.put("Brian", "1234567");
+		map.put("Alex", "2345678");
+		map.put("Ali", "3456789");
+		map.put("Robert", "4567890");
+		
+		
+	}
+	
+	public void add(String name, String number) {
+		
+	}
+	
+	public void remove(String name) {
+		//bla bla bla 
+		
+	}
+	
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
