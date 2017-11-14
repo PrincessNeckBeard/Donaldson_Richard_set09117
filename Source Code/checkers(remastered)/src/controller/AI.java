c6a014496df835cfcba3eb98d04ac3d4b251371f
Richard Donaldson <bearofthesea@gmail.com>
2017-11-14 01:26:43 +0000

    Added folders/renamed folders for submission

---
+++ Source Code/checkers(remastered)/src/controller/AI.java
@@ -0,0 +1,41 @@
+package controller;
+
+import java.util.LinkedList;
+
+import model.Board;
+import model.Move;
+
+public class AI {
+LinkedList<Move> AIPossibleMoves = new LinkedList<Move>();
+Board board = new Board();
+Game gameController = new Game();
+AI AIController = new AI();
+
+
+
+public void minimax() {
+	
+}
+
+
+
+
+public LinkedList<Move> getPossibleMoves() {
+	
+	
+	
+	
+	return AIPossibleMoves;
+	
+}
+
+
+public static void main(String args[]) {
+	
+	
+}
+
+
+
+
+}
